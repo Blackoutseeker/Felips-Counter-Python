aeeccadf7a5b63fbb8c2337fd5a85a19af9dc269
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-11-06 21:06:48 -0300

    feat: Created __init__.py file in felips_counter

---
+++ felips_counter/__init__.py
@@ -0,0 +1,3 @@
+__version__: str = '1.0.10'
+default_ignore_patterns: [str] = ['/.idea', '/__pycache__', '/node_modules', '/dist', '/build', '/out',
+                                  '/assets', '/coverage', '/.vscode', '.gitignore', '*.lock', '*.env']
